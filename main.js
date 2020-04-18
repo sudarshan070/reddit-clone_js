b0522f29115e988b8e61876ebb0bd9485d0a7aa6
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-04-18 12:32:04 +0530

    get  function data

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-fetch(`https://api.reddit.com/r/javascript`)
-  .then((res) => res.json())
-  .then((res) => console.log(res));
+var input = document.querySelector(".input");
+
+function getData(event) {
+  if (event.keyCode == 13 && input.value.trim() != "") {
+    fetch(`https://api.reddit.com/r/${input.value}`)
+      .then((res) => res.json())
+      .then((res) => viewData(res.data.children));
+  }
+}
+
+function viewData(data) {
+  console.log(data);
+}
+
+input.addEventListener("keyup", getData);
